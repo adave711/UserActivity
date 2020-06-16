3a44bd31b96e012043918884556c4125fb3ec3f6
Ami Dave <amidave80@gmail.com>
2020-06-16 14:44:18 -0400

    change file

--- index.js
+++ index.js
@@ -11,26 +11,14 @@ var app = express();
 
 //app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://gis.streetlogix.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  // res.header(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  // );
-  next();
-});
+app.use(cors({ origin: "*" }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.post("/api/users/add_activities", function (req, res, next) {
+app.post("/api/users/add_activities", function (req, res) {
   var sessionid = uuid.genUuid().substring(0, 8);
 
   dao.connect();
@@ -86,7 +74,7 @@ app.post("/api/users/add_activities", function (req, res, next) {
 //   );
 // });
 
-app.get("/", function (req, res, next) {
+app.get("/", function (req, res) {
   res.send("api is running");
 });
 
